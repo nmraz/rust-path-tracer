916fa4c94b70d682fcac88dd5c60073d86d2549f
Noam Raz <noamraz8@gmail.com>
2019-11-05 21:24:44 +0200

    Update renderer options

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,30 +1,10 @@
 use crate::math::Vec3;
 
-struct SampledPixel {
-    total: Vec3,
-    count: usize,
-}
-
-impl SampledPixel {
-    pub fn new() -> SampledPixel {
-        SampledPixel {
-            total: Default::default(),
-            count: 0,
-        }
-    }
-
-    pub fn add_sample(&mut self, color: Vec3) {
-        self.total = self.total + color; // TODO: std::ops::AddAssign et al for Vec3
-        self.count += 1;
-    }
-
-    pub fn result(&self) -> Vec3 {
-        if self.count == 0 {
-            self.total
-        } else {
-            self.total / (self.count as f64)
-        }
-    }
+pub struct CameraOptions {
+    pub pos: Vec3,
+    pub looking_at: Vec3,
+    pub up: Vec3,
+    pub vert_fov: f64,
 }
 
 pub struct RenderOptions {
@@ -32,4 +12,5 @@ pub struct RenderOptions {
     pub height: usize,
     pub samples_per_pixel: u32,
     pub max_depth: u32,
+    pub camera_options: CameraOptions,
 }
