bd688b673aa8fd8e172802662c4515309831d37c
Noam Raz <noamraz8@gmail.com>
2019-11-09 16:45:25 +0200

    Update rendered scene

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,32 @@ use renderer::*;
 use scene::*;
 
 fn main() {
-    let width = 300;
+    let width = 400;
     let height = 200;
 
-    let max_depth = 5;
-    let spp = 100;
+    let max_depth = 10;
+    let spp = 1000;
 
     let s = Scene::with_primitives(vec![
         Primitive::new(
             Sphere::new(
                 Vec3 {
-                    x: 0.0,
+                    x: 2.5,
+                    y: 0.0,
+                    z: -5.0,
+                },
+                2.0,
+            ),
+            Material::Diffuse(Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            }),
+        ),
+        Primitive::new(
+            Sphere::new(
+                Vec3 {
+                    x: -2.5,
                     y: 0.0,
                     z: -5.0,
                 },
@@ -34,22 +49,67 @@ fn main() {
             Material::Diffuse(Vec3 {
                 x: 1.0,
                 y: 0.0,
-                z: 0.0,
+                z: 1.0,
             }),
         ),
         Primitive::new(
             Sphere::new(
                 Vec3 {
                     x: 0.0,
-                    y: 1.0,
+                    y: -12.0,
                     z: -3.0,
                 },
-                0.5,
+                10.0,
+            ),
+            Material::Diffuse(Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            }),
+        ),
+        Primitive::new(
+            Sphere::new(
+                Vec3 {
+                    x: -3.0,
+                    y: -5.0,
+                    z: -2.0,
+                },
+                3.0,
+            ),
+            Material::Light(Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.5,
+            }),
+        ),
+        Primitive::new(
+            Sphere::new(
+                Vec3 {
+                    x: 0.0,
+                    y: 8.0,
+                    z: -3.0,
+                },
+                6.0,
             ),
             Material::Light(Vec3 {
                 x: 1.0,
                 y: 1.0,
                 z: 1.0,
+            }),
+        ),
+        Primitive::new(
+            Sphere::new(
+                Vec3 {
+                    x: 0.0,
+                    y: 0.0,
+                    z: 7.0,
+                },
+                6.0,
+            ),
+            Material::Light(Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 0.0,
             }),
         ),
     ]);
@@ -66,7 +126,7 @@ fn main() {
             y: 1.0,
             z: 0.0,
         },
-        vert_fov: 60.0,
+        vert_fov: 55.0,
     };
     let cam = Camera::new(&opts, width, height);
     let mut pixels = vec![Vec3::default(); (width * height) as usize].into_boxed_slice();
