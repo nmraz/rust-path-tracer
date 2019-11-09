ff4134a992791c6f9ccf72df167f67a6c97dde36
Noam Raz <noamraz8@gmail.com>
2019-11-09 13:34:23 +0200

    Update main to render basic scene

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,53 @@ mod scene;
 use std::fs::File;
 use std::io::*;
 
+use rand::Rng;
+
 use geom::*;
 use math::*;
 use renderer::*;
 use scene::*;
 
 fn main() {
+    let width = 300;
+    let height = 200;
+
+    let max_depth = 5;
+    let spp = 100;
+
+    let s = Scene::with_primitives(vec![
+        Primitive::new(
+            Sphere::new(
+                Vec3 {
+                    x: 0.0,
+                    y: 0.0,
+                    z: -5.0,
+                },
+                2.0,
+            ),
+            Material::Diffuse(Vec3 {
+                x: 1.0,
+                y: 0.0,
+                z: 0.0,
+            }),
+        ),
+        Primitive::new(
+            Sphere::new(
+                Vec3 {
+                    x: 0.0,
+                    y: 1.0,
+                    z: -3.0,
+                },
+                0.5,
+            ),
+            Material::Light(Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            }),
+        ),
+    ]);
+
     let opts = CameraOptions {
         pos: Vec3::default(),
         target: Vec3 {
@@ -27,36 +68,23 @@ fn main() {
         },
         vert_fov: 60.0,
     };
-
-    let width = 300;
-    let height = 200;
-
-    let prim = Primitive::new(
-        Sphere::new(
-            Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: -5.0,
-            },
-            2.0,
-        ),
-        Material::Diffuse(Vec3 {
-            x: 1.0,
-            y: 0.0,
-            z: 0.0,
-        }),
-    );
     let cam = Camera::new(&opts, width, height);
     let mut pixels = vec![Vec3::default(); (width * height) as usize].into_boxed_slice();
 
+    let mut rng = rand::thread_rng();
+
     for y in 0..height {
         for x in 0..width {
-            let ray = cam.cast_ray(f64::from(x) + 0.5, f64::from(y) + 0.5);
-            if prim.geom().intersect(&ray).is_some() {
-                if let Material::Diffuse(ref color) = prim.material() {
-                    pixels[(x + y * width) as usize] = *color;
-                }
-            }
+            let total_sampled = (0..spp)
+                .map(|_| {
+                    let ray = cam.cast_ray(
+                        f64::from(x) + rng.gen::<f64>(),
+                        f64::from(y) + rng.gen::<f64>(),
+                    );
+                    trace_ray(&s, &ray, &mut rng, 0, max_depth)
+                })
+                .fold(Vec3::default(), |acc, val| acc + val);
+            pixels[(x + y * width) as usize] = total_sampled / f64::from(spp);
         }
     }
 
