a77ce49f28aa407b64a2190bee3de5ec6d66fef9
Noam Raz <noamraz8@gmail.com>
2019-11-13 23:13:32 +0200

    Factor basis computation out of hemisphere sampler

--- src/sample.rs
+++ src/sample.rs
@@ -4,26 +4,40 @@ use rand::Rng;
 
 use crate::math::{Unit3, Vec3};
 
+struct Basis {
+    pub x: Vec3,
+    pub y: Vec3,
+    pub z: Vec3,
+}
+
+impl Basis {
+    pub fn from_normal(normal: Unit3) -> Basis {
+        let z: Vec3 = normal.into();
+
+        // We need another vector that isn't collinear with the normal.
+        let other = if z.x.abs() > 0.1 {
+            Vec3 {
+                x: 0.0,
+                y: 1.0,
+                z: 0.0,
+            }
+        } else {
+            Vec3 {
+                x: 1.0,
+                y: 0.0,
+                z: 0.0,
+            }
+        };
+
+        let x = Vec3::from(other.cross(z).to_unit());
+        let y = z.cross(x);
+
+        Basis { x, y, z }
+    }
+}
+
 pub fn sample_cos_weighted_hemisphere<R: Rng + ?Sized>(normal: Unit3, rng: &mut R) -> Unit3 {
-    let basis_z: Vec3 = normal.into();
-
-    // We need another vector that isn't collinear with the normal.
-    let other = if basis_z.x.abs() > 0.1 {
-        Vec3 {
-            x: 0.0,
-            y: 1.0,
-            z: 0.0,
-        }
-    } else {
-        Vec3 {
-            x: 1.0,
-            y: 0.0,
-            z: 0.0,
-        }
-    };
-
-    let basis_x = Vec3::from(other.cross(basis_z).to_unit());
-    let basis_y = basis_z.cross(basis_x);
+    let basis = Basis::from_normal(normal);
 
     let radius_squared: f64 = rng.gen();
     let theta: f64 = rng.gen_range(0.0, 2.0 * f64::consts::PI);
@@ -33,5 +47,5 @@ pub fn sample_cos_weighted_hemisphere<R: Rng + ?Sized>(normal: Unit3, rng: &mut 
     let y = radius * theta.sin();
     let z = (1.0 - radius_squared).sqrt();
 
-    Unit3::from_unit_vec3(x * basis_x + y * basis_y + z * basis_z)
+    Unit3::from_unit_vec3(x * basis.x + y * basis.y + z * basis.z)
 }
